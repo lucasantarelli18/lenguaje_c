a4abcd179de35f6a2585634ff3ac98aa6d5b197a
Luca Santarelli <49928989+lucasantarelli18@users.noreply.github.com>
2019-04-29 20:33:43 -0300

    Sintaxis función SWITCH

---
+++ 02.2-Caso.c
@@ -0,0 +1,29 @@
+﻿//EJEMPLO DE CASO
+
+#include <stdio.h> //Permite usar printf y scanf
+#include <conio.h> //Permite usar _getch
+
+int numero; //Declaramos un entero
+
+int main(void){
+	printf("Ingrese un numero entero del 1 al 3: ");
+	scanf("%d", &numero);
+	printf("=======Numero ingresado correctamente======= \n");
+	//Le damos valor al entero
+	switch (numero){
+		case 1:
+			printf("El numero elegido es 1"); //Se ejecuta si el numero es 1
+			break;
+		case 2:
+			printf("El numero elegido es 2"); //Se ejecuta si el numero es 2
+			break;
+		case 3:
+			printf("El numero elegido es 3"); //Se ejecuta si el numero es 3
+			break;
+		default:
+			printf("Numero incorrecto"); //Se ejecuta si el numero es distinto del 1 al 3
+			break;
+	}
+	_getch();
+	return 0;
+}
