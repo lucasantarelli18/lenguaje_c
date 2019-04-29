1b1d407cc8c525bcf533430cb0c00f14f7550e77
Luca Santarelli <49928989+lucasantarelli18@users.noreply.github.com>
2019-04-29 20:32:14 -0300

    Sintaxis función IF

---
+++ 02.1-Decision simple.c
@@ -0,0 +1,26 @@
+﻿//EJEMPLO DE DECISION SIMPLE EN C
+
+#include <stdio.h> //Permite usar printf y scanf
+#include <conio.h> //Permite usar _getch
+
+int numero1, numero2; //Declaramos 2 variables enteras
+
+int main(void){
+	printf("Ingrese un numero entero: ");
+	scanf("%d", &numero1);
+	printf("=======Numero ingresado correctamente======= \n");
+	printf("Ingrese otro numero entero: ");
+	scanf("%d", &numero2);
+	printf("=======Numero ingresado correctamente======= \n");
+	_getch();
+	//Le dimos valores a ambos enteros
+
+	if (numero1>=numero2){ 
+		printf("El numero %d es mayor o igual que el numero %d", numero1, numero2); //si la condicion se cumple se ejecutara este bloque
+	}
+	else{ 
+		printf("El numero %d es menor que el numero %d", numero1, numero2); //si la condicion no se cumple se ejecutara este bloque
+	}
+	_getch();
+	return 0;
+}
