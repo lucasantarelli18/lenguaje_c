49f0379d69927367febeeda5d949d7fbcc359f38
Luca Santarelli <49928989+lucasantarelli18@users.noreply.github.com>
2019-05-09 20:10:44 -0300

    Sintaxis de Arreglos en C

---
+++ 04-Arreglos.c
@@ -0,0 +1,24 @@
+﻿//EJEMPLO ARREGLO (TIPO VECTOR)
+
+#include <stdio.h> 
+#include <conio.h> 
+ 
+int vecNum[5], i;  //Defino un arreglo tipo vector de enteros de 5 posiciones y un índice 
+ 
+int main(){  
+	for (i=0; i<5; i++){ //Recorro el arreglo
+   		vecNum[i]= 0; //Inicializo con valor 0
+    }  
+	for (i=0; i<5; i++){     //Realizo la carga del vector
+    	printf("Vector posicion: %d\n", i);
+		printf("Ingrese valor entero: ");
+    	scanf("%d", &vecNum[i]); //Nótese que en el scan el vector va con & porque use índice   
+		_clrscr();
+    }
+	for (i=0; i<5; i++){ //Muestro como quedo el vector
+		printf("Vector Posicion: %d\n", i);
+		printf("Valor: %d\n",vecNum[i]);
+		printf("===============\n");
+	}
+	_getch();
+}       
